32079ff46588375553280abfc9366b679c33acda
Testerbens <105905408+Testerbens@users.noreply.github.com>
2022-06-12 17:05:59 +0700

    Update Cobacoin.sh

--- Cobacoin.sh
+++ Cobacoin.sh
@@ -2,4 +2,4 @@
 Sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u 3EG2npqCpv75koqhUc37E9aR3W8aTyqbeM.Terter_ben -p c=BTC x --cpu 3
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RC4fCnH1ritAY1NPP8omSzoL55EtK5gYJa -p c=RVN x --cpu 3
